cb5eb695bd31e0510d1d2ab1bf2e76b1fc8d8b21
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2023-07-11 13:00:55 +0600

    first commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const ViewBoxesWithColorAndText = () => {
+    return (
+        <View>
+            <Text>This is demo Project</Text>
+        </View>
+    );
+};
+
+export default ViewBoxesWithColorAndText;
